c672ff3ef06c7d2997a3db3259c84a3eed638929
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-09-20 20:11:39 +0600

    Add initial implementation of doubly linked list and singly linked list

---
+++ singly_linked_list_sub.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int val;
+        Node* next;
+
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+
+
+    }
+};
+
+void Insert_at_head()
+int main() {
+    
+    return 0;
+}
