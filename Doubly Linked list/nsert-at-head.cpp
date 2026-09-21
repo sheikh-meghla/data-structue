3a655dc47f1450aae590f2a1b6f0f06bab0057f7
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-09-21 19:42:35 +0600

    Refactor code structure for improved readability and maintainability

---
+++ Doubly Linked list/nsert-at-head.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+        int val;
+        Node* next;
+        Node* prev;
+    Node(int val){
+        this ->val = val;
+        this ->next = NULL;
+        this->prev = NULL;
+    }
+
+};
+
+void print_forward(Node* head){
+
+    Node* tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val<<" ";
+        tmp = tmp->next;
+
+    }
+    cout << endl;
+
+}
+
+void Insert_at_head(Node* &head,int val){
+    Node* newnode = new Node(val);
+    newnode->next = head;
+    head->prev = newnode;
+    head = newnode;
+
+}
+
+
+int main() {
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* b = new Node(30);
+
+    head->next = a;
+    a->prev = head;
+    
+    a->next = b;
+    b->prev = a;
+
+    Insert_at_head(head,100);
+    print_forward(head);
+    return 0;
+}
