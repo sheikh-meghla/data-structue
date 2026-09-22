2753eb738a8f6d77cabd9e85d049c0320cfb1221
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-09-22 18:00:12 +0600

    Refactor code structure for improved readability and maintainability

---
+++ Doubly Linked list/Delete-at-head.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+        int val;
+        Node* next;
+        Node* prev;
+    Node(int val){
+        this ->val = val;
+        this ->next = NULL;
+        this->prev = NULL;
+    }
+
+};
+
+void print_forward(Node* head){
+
+    Node* tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val<<" ";
+        tmp = tmp->next;
+
+    }
+    cout << endl;
+
+}
+
+void Delete_at_head(Node* &head,Node* &tail ){
+    Node* delete_node = head;
+    head = head->next;
+    delete delete_node;
+    if (head == NULL)
+    {
+        tail = NULL;
+        return;
+    }
+    head->prev = NULL;
+   
+}
+
+
+int main() {
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* tail = new Node(30);
+
+    head->next = a;
+    a->prev = head;
+    
+    a->next = tail;
+    tail->prev = a;
+
+
+    Delete_at_head(head,tail);
+    print_forward(head);
+    return 0;
+}
