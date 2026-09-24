dd160806e03a10f2eca782f499a72b9e695e5228
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-09-24 22:04:59 +0600

    Refactor code structure for improved readability and maintainability

---
+++ Doubly Linked list/Range_Maximum_Doubly_Linked_List.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* prev;
+
+    Node(int val) {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void print_forward(Node* head,int x,int y) {
+    Node* tmp = head;
+    long long sum = 0;
+    int idx = 0;
+    int mx = INT_MIN;
+    while (tmp != NULL) {
+        
+        if (idx >= x && idx <= y)
+        {
+            if (mx < tmp->val)
+            {
+                mx = tmp->val;
+            }
+            
+        }
+        idx++; 
+        tmp = tmp->next;
+    }
+    cout << mx << endl;
+
+}
+
+void Insert_at_tail(Node* &head, Node* &tail, int val) {
+    Node* newnode = new Node(val);
+
+    if (head == NULL) {
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+
+    tail->next = newnode;
+    newnode->prev = tail;
+    tail = newnode;
+}
+
+int main() {
+    Node* head = NULL;
+    Node* tail = NULL;
+    int x,y;
+    cin >> x >> y;
+    int val;
+
+    while (true) {
+        cin >> val;
+
+        if (val == -1) {
+            break;
+        }
+
+        Insert_at_tail(head, tail, val);
+    }
+
+
+    print_forward(head,x,y);
+
+    return 0;
+}
