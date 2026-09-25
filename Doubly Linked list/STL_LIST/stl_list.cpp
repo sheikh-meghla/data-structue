ef9243b34e9d6f9954238f4480c340e1ca4b0560
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-09-25 09:39:49 +0600

    Update stl_list.cpp to uncomment list initialization and improve code clarity

--- Doubly Linked list/STL_LIST/stl_list.cpp
+++ Doubly Linked list/STL_LIST/stl_list.cpp
@@ -3,12 +3,20 @@ using namespace std;
 
 int main() {
     
-    list<int>l(5,1);
 
+    // list<int>l(5,1);
     // for(auto it = l.begin(); it != l.end(); it++){
     //     cout << *it << endl;
     // }
-    for(int val : l){
+
+
+    // copy vector to list
+    vector<int> v = {10,20,30,40};
+    list<int> l2(v.begin(),v.end());
+    // l2.clear();
+
+    l2.resize(8,1);
+    for(int val : l2){
         cout << val <<endl; 
     }
     return 0;
