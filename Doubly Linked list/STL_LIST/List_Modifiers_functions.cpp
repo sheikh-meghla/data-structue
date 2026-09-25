36b7ba1f7ab551fd2e69becb5c5edfda2e2bee12
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-09-25 10:25:39 +0600

    Add list element manipulation with push_back and push_front in List_Modifiers_functions.cpp

--- Doubly Linked list/STL_LIST/List_Modifiers_functions.cpp
+++ Doubly Linked list/STL_LIST/List_Modifiers_functions.cpp
@@ -7,6 +7,10 @@ int main() {
     list<int> l2;
     l2 = l;
     // l2.assign(l.begin(),l.end());
+
+
+    l2.push_back(6);
+    l2.push_front(0);
     for(int val : l2){
         cout << val << " ";
     }
