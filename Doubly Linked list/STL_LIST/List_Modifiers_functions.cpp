8870b3f28d4826d8fc65a57c675ea9f1a440e258
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-09-25 18:17:46 +0600

    Add list initialization and modify insertion operation in List_Modifiers_functions.cpp

--- Doubly Linked list/STL_LIST/List_Modifiers_functions.cpp
+++ Doubly Linked list/STL_LIST/List_Modifiers_functions.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() {
     
     list<int> l = {1,2,3,4,5};
+    list<int> l3 = {7,8,9,1};
     list<int> l2;
     l2 = l;
     // l2.assign(l.begin(),l.end());
@@ -14,7 +15,8 @@ int main() {
     // l2.pop_back();
     // l2.pop_front();
     // cout << *next(l2.begin(),2)<<endl;
-    l.insert(next(l2.begin(),2),100);
+    // l.insert(next(l2.begin(),2),100);
+    l.insert(next(l2.begin(),2),l3.begin(),l3.end());
     for(int val : l2){
         cout << val << " ";
     }
